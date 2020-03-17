8fb76fb82754e50a40275918a644a6782b1bbb83
flavien <Flavien.Alonzo@ec-nantes.fr>
2020-03-17 11:06:39 +0100

    17/03

--- matrice_tri.m
+++ matrice_tri.m
@@ -150,8 +150,8 @@ function [A,b] = matrice_tri(Tri,f,g,h,equation)
     elseif equation==5
         A = sparse(NbTri,NbTri);
         b = sparse(NbTri,1);
-        D = 1.05e-3;
-        r = 1.07e-3;
+        D = 5e-2;
+        r = 0;
         wbm = waitbar(0,'Building A and b');
         for i=1:NbTri       
            waitbar(i/NbTri,wbm);
@@ -181,6 +181,7 @@ function [A,b] = matrice_tri(Tri,f,g,h,equation)
                end
            end
         end
+        
     end
     close(wbm);
 end
