2c003bd30b106908c2b0f7e3d9b06fb5d9d5e45e
flavien <Flavien.Alonzo@ec-nantes.fr>
2020-03-22 20:07:01 +0100

    fonctionnelle

--- fonctionnelle.m
+++ fonctionnelle.m
@@ -1,14 +1,12 @@
 function F = fonctionnelle(x,f,f_prime,g,g_prime,h,h_prime,TN,Coef_trans,Volume,C,Delta_t,U)
     F = zeros(size(TN,1),1);
     for i=1:size(TN,1)
-        F(i,1) = -h(x(i))*Delta_t+x(i)-U(i);
+    F(i,1) = x(i) - Delta_t*h(x(i)) - U(i);
         for j=1:size(TN,2)
             if isnan(TN(i,j))
                 
             else
-                F(i,1) = F(i,1)+(x(i)-x(TN(i,j)))*Coef_trans(i,j)*0.5*(f(x(i))+f(x(TN(i,j))))*Delta_t/Volume(1,i) ...
-                    + Coef_trans(i,j)*g(x(i))*(C(TN(i,j))-C(i))*(C(TN(i,j))-C(i)>0)*Delta_t/Volume(1,i)...
-                    + Coef_trans(i,j)*g(x(TN(i,j)))*(C(TN(i,j))-C(i))*(C(TN(i,j))-C(i)<0)*Delta_t/Volume(1,i);
+                F(i,1) = F(i,1) + Delta_t/Volume(1,i)*Coef_trans(i,j)*(f(x(i))-f(x(TN(i,j)))) + Delta_t/Volume(1,i)*Coef_trans(i,j)*0.5*(g(x(i))+g(x(TN(i,j))))*max(C(TN(i,j),1)-C(i,1),0)-Delta_t/Volume(1,i)*Coef_trans(i,j)*0.5*(g(x(i))+g(x(TN(i,j))))*max(C(i,1)-C(TN(i,j),1),0);
             end
         end
     end
